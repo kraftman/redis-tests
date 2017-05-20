f3feb25e11ba8da5f638a371ade5f1c41d4b69a2
Chris Tanner <me@itschr.is>
2017-05-20 15:31:41 +0100

    add pipelined stats

--- app.lua
+++ app.lua
@@ -22,6 +22,7 @@ local function gettimeofday()
  	return tonumber(gettimeofday_struct.tv_sec) * 1000000 + tonumber(gettimeofday_struct.tv_usec)
 end
 
+
 app:get("/", function()
   return "Welcome to Lapis " .. require("lapis.version")
 end)
@@ -29,6 +30,7 @@ end)
 local function RunTests(host, port)
   -- run 10000 operations
   -- count how long it took
+  ngx.say(host, ' ', port, ':</br>')
 
   local red = redis:new()
   local ok, err  = red:connect(host, port)
@@ -37,23 +39,64 @@ local function RunTests(host, port)
     return
   end
   local count = 10000
+
   local a = gettimeofday()
+  local a1 = ngx.time()
   for i = 1, count do
     red:set('key'..i,'value'..i)
   end
   local b = gettimeofday()
-  print(count/(b-a)..' requests per second')
+
+  ngx.say(count/((b-a)/1000000)..' requests per second</br>')
+
   for i = 1, count do
     red:get('key'..i)
   end
-
-
-
+  a = gettimeofday()
+  ngx.say(count/((a-b)/1000000)..' deletes per second</br>')
 
 end
 
+local function RunPiped(host, port)
+
+  ngx.say('pipelined: ', host, ' ', port, ':</br>')
+
+	local red = redis:new()
+  local ok, err  = red:connect(host, port)
+  if not ok then
+    ngx.say('failed to connect')
+    return
+  end
+  local count = 0
+  local a = gettimeofday()
+
+  for i = 1, 1000 do
+	  red:init_pipeline()
+
+	  for i = 1, 1000 do
+	  	count = count +1
+	    red:set('key'..i,'value'..i)
+	  end
+	  local ok, err = red:commit_pipeline()
+	  if ok then
+	  	--print('success')
+	  else
+	  	--print(err)
+	  end
+	end
+
+  local b = gettimeofday()
+  ngx.say('pipelined: ', count/((b-a)/1000000),'requests per second </br>')
+
+end
+
+
+
 app:get('/spiped',function()
-  RunTests('163.172.177.164', 6379)
+  RunTests('127.0.0.1', 6379)
+  RunTests('127.0.0.1', 16379)
+  RunPiped('127.0.0.1',6379)
+  RunPiped('127.0.0.1',16379)
 end)
 
 return app
